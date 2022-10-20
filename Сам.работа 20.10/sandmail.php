efffcefe9422d28622b76682b551cc51da649468
GorbunovArtem <106732889+GorbunovArtem@users.noreply.github.com>
2022-10-20 14:51:52 +0500

    Add files via upload

---
+++ Сам.работа 20.10/sandmail.php
@@ -0,0 +1,31 @@
+<?php
+  if($_POST['capcha'] != 56){
+    header('location: index.php');
+    exit;
+  }
+
+  if($_POST['subject'] == 1){
+    $subject = "Вопрос по уроку";
+  } elseif($_POST['subject'] == 2){
+    $subject = "Личный вопрос";
+  } elseif($_POST['subject'] == 3){
+    $subject = "Благодарность";
+  } else {
+    $subject = "Вопрос по уроку";
+  }
+
+  $to = "4A58B92C@mail.ru";
+  $from = trim($_POST['email']);
+
+  $message = htmlspecialchars($_POST['message']);
+  $message = urldecode($message);
+  $message = trim($massage);
+
+  $headers = "From: $from" . "\r\n" . "Replay-To: $from" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+
+  if(mail($to, $subject, $massage, $headers)){
+    echo "Письмо отправлено";
+  } else {
+    echo "Письмо не отправлено";
+  }
+?>
